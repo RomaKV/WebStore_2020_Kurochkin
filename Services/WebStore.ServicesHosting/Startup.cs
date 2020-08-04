a8516b26cc3218c6ba0465dc41d47cbd802559ce
Roma <Roma@DESKTOP-O51730H>
2020-08-04 17:35:19 +0300

    ServicesHosting: Добавлен Entity Framework

--- Services/WebStore.ServicesHosting/Startup.cs
+++ Services/WebStore.ServicesHosting/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Services.WebStore.DAL;
 
 namespace Services.WebStore.ServicesHosting
 {
@@ -26,6 +28,12 @@ namespace Services.WebStore.ServicesHosting
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<WebStoreContext>(options => options
+                .UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
