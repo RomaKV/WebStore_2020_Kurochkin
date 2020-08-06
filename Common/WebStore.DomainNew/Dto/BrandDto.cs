9902b896ba97d2858979b7eb5f06c217f6ba388d
Roma <Roma@DESKTOP-O51730H>
2020-08-06 14:43:09 +0300

    2-2 Созданы Dto для Product и Brand

---
+++ Common/WebStore.DomainNew/Dto/BrandDto.cs
@@ -0,0 +1,10 @@
+﻿using Common.WebStore.Domain.Entities.Base.Interfaces;
+
+namespace Common.WebStore.DomainNew.Dto
+{
+    public class BrandDto : INamedEntity
+    {
+        public int Id { get; set;}
+        public string Name { get; set; }
+    }
+}
---
+++ Common/WebStore.DomainNew/Dto/ProductDto.cs
@@ -0,0 +1,17 @@
+﻿using Common.WebStore.Domain.Entities.Base.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.WebStore.DomainNew.Dto
+{
+    class ProductDto : INamedEntity, IOrderedEntity
+    {
+        public int Order { get; set;}
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public BrandDto Brand { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
