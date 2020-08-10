1ef70ede193e1b748a224272757a2ac16963df09
Roma <Roma@DESKTOP-O51730H>
2020-08-10 15:46:16 +0300

    2-3 Созданы Dto для работы с сервисом user

---
+++ Common/WebStore.DomainNew/Dto/User/SetLockoutDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.WebStore.DomainNew.Dto.User
+{
+    public class SetLockoutDto
+    {
+        Entities.User User { get; set; }
+        public DateTimeOffset? LockoutEnd { get; set; }
+    }
+}
