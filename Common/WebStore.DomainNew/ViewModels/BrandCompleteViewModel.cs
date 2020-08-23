b873eee95aa246fa00c2f8f3580c0867891d1308
Roma <Roma@DESKTOP-O51730H>
2020-08-23 13:14:41 +0300

    2-6 Реализована подсветка Brand

---
+++ Common/WebStore.DomainNew/ViewModels/BrandCompleteViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.WebStore.ViewModels;
+
+namespace Common.WebStore.ViewModels
+{
+    public class BrandCompleteViewModel
+    {
+        public IEnumerable<BrandViewModel> Brands { get; set; }
+
+        public int? CurrentBrandId { get; set; }
+    }
+}
