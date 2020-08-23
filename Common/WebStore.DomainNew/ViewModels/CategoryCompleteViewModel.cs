dd361a88be1eec163684af0e7d15bba86df9d336
Roma <Roma@DESKTOP-O51730H>
2020-08-23 12:03:27 +0300

    2-6 Реализована подсветка категорий

---
+++ Common/WebStore.DomainNew/ViewModels/CategoryCompleteViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.WebStore.ViewModels;
+
+namespace Common.WebStore.DomainNew.ViewModels
+{
+    public class CategoryCompleteViewModel
+    {
+        public  IEnumerable<CategoryViewModel> Category { get; set; }
+
+        public int? CurrentParentCategoryId { get; set; }
+
+        public int? CurrentCategoryId { get; set; }
+
+    }
+}
