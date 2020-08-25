6ade976a873e037f115c3071a678a6710ed72c8a
Roma <Roma@DESKTOP-O51730H>
2020-08-26 02:05:04 +0300

    2- 7 Добавлен асинхронный подсчет общей суммы в корзине

--- UI/WebStore_2020/wwwroot/js/cartLogic.js
+++ UI/WebStore_2020/wwwroot/js/cartLogic.js
@@ -49,6 +49,7 @@
         $.get(Cart._properties.removeFromCartLink + '/' + id)
             .done(function() {
                 button.closest('tr').remove();
+                Cart.refreshPrice(container);
                 Cart.refreshCartView();
             })
             .fail(function() {
@@ -59,13 +60,25 @@
     },
 
     refreshPrice: function(container) {
-        var quantity = parseInt($('cart_quantity_input', container).val());
-        var price = parseFloat($('cart_price', container).data('rice'));
+        var quantity = parseInt($('.cart_quantity_input', container).val());
+        var price = parseFloat($('.cart_price', container).data('price'));
         var totalPrice = quantity * price;
         var value = totalPrice.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
 
         $('.cart_total_price', container).data('price', totalPrice);
         $('.cart_total_price', container).html(value);
+        Cart.refreshTotalPrice();
+    },
+
+    refreshTotalPrice: function() {
+        var total = 0;
+        $('.cart_total_price').each(function() {
+            var price = parseFloat($(this).data('price'));
+            total += price;
+        });
+
+        var value = total.toLocaleString('en-Us', { style: 'currency', currency: 'USD' });
+        $('#totalOrderSum').html(value);
     },
 
     incrementItem: function(event) {
@@ -77,6 +90,7 @@
         $.get(Cart._properties.addToCartLink + '/' + id).done(function() {
             var value = parseInt($('.cart_quantity_input', container).val());
             $('.cart_quantity_input', container).val(value + 1);
+            Cart.refreshPrice(container);
             Cart.refreshCartView();
         }).fail(function() { console.log('incrementItem error'); });
     },
@@ -91,20 +105,24 @@
             value = value - 1;
             if (value > 0) {
                 $('.cart_quantity_input', container).val(value);
+                Cart.refreshPrice(container);
+                Cart.refreshCartView();
             } else {
                 $.get(Cart._properties.removeFromCartLink + '/' + id)
                     .done(function () {
                         button.closest('tr').remove();
+                        Cart.refreshPrice(container);
                         Cart.refreshCartView();
                     })
                     .fail(function () {
                             console.log('removeFromCart error');
 
                         }
-                    );
+                );
+
             }
 
-            Cart.refreshCartView();
+           
         }).fail(function () { console.log('incrementItem error'); });
     }
 
